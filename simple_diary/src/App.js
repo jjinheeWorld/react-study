a291caca4083aff83742222656de7f8d08c1a5c5
jjinheeWorld <wlsgml450@naver.com>
2023-09-13 18:02:44 +0900

    React에서 배열 사용하기(데이터 추가하기)

--- simple_diary/src/App.js
+++ simple_diary/src/App.js
@@ -1,36 +1,53 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+import {useState, useRef} from 'react';
 
-const dummyList = [
-  {
-    id: 1,
-    author: '이진희',
-    content: '안농 👋 나는 귀염둥이 진희야',
-    emotion: 5,
-    created_date: new Date().getTime()
-  },
-  {
-    id: 2,
-    author: '코코몽',
-    content: '안농 👋 나는야 코코몽',
-    emotion: 4,
-    created_date: new Date().getTime()
-  },
-  {
-    id: 3,
-    author: '푸바오',
-    content: '안농 👋 나는 푸바오야',
-    emotion: 2,
-    created_date: new Date().getTime()
-  },
-]
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: '이진희',
+//     content: '안농 👋 나는 귀염둥이 진희야',
+//     emotion: 5,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id: 2,
+//     author: '코코몽',
+//     content: '안농 👋 나는야 코코몽',
+//     emotion: 4,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id: 3,
+//     author: '푸바오',
+//     content: '안농 👋 나는 푸바오야',
+//     emotion: 2,
+//     created_date: new Date().getTime()
+//   },
+// ]
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id : dataId.current
+    }
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  }
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
