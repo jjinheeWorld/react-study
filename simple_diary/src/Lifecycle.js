638dc725aae965793bb981cc8e315533d617e226
jjinheeWorld <wlsgml450@naver.com>
2023-09-14 18:17:36 +0900

    React Lifecycle 제어하기(useEffect)

---
+++ simple_diary/src/Lifecycle.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+const UnmountTest = () => {
+  useEffect(() => {
+    console.log('Mount!');
+
+    return () => {
+      // Unmount 시점에 실행되게 됨
+      console.log('Unmount!');
+    };
+  }, []);
+  return <div>Unmount Testing Component</div>;
+};
+
+const Lifecycle = () => {
+  const [isVisible, setIsvisible] = useState(false);
+  const toggle = () => setIsvisible(!isVisible);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && <UnmountTest />}
+    </div>
+  );
+};
+
+export default Lifecycle;
